6b9bdb83cedc84b6923284bd172a3335c74b39fd
David Bayón Alvarez <david.bayon.alvarez@mobgen.com>
2019-12-11 12:22:50 +0100

    Added sum function

--- StringCalculator/ViewController.swift
+++ StringCalculator/ViewController.swift
@@ -18,3 +18,9 @@ class ViewController: UIViewController {
 
 }
 
+class Calculator {
+    func add(numbers: String) -> Int {
+        numbers.components(separatedBy: ",").compactMap { Int($0) }.reduce(0, +)
+    }
+}
+
--- StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculatorTests/StringCalculatorTests.swift
@@ -11,24 +11,10 @@ import XCTest
 
 class StringCalculatorTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_sum() {
+        let sut = Calculator()
+        
+        XCTAssert(sut.add(numbers: "1,2,3") == 6)
     }
 
 }
