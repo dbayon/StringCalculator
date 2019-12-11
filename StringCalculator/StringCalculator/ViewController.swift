c11119ff972ffd9ca53a2b30da1c9ba126281834
David Bayón Alvarez <david.bayon.alvarez@mobgen.com>
2019-12-11 11:44:58 +0100

    first

---
+++ StringCalculator/StringCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by David Bayón Alvarez on 11/12/2019.
+//  Copyright © 2019 Mobgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
